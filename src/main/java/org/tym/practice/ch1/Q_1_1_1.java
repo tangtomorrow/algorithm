a502655a77fba37828224546d21d5087164926a6
tangtomorrow <tangyaming_nju@sina.com>
2016-11-02 20:30:33 +0800

    adjust

--- src/main/java/org/tym/practice/ch1/Q_1_1_1.java
+++ src/main/java/org/tym/practice/ch1/Q_1_1_1.java
@@ -1,4 +1,4 @@
-package org.tym.practice;
+package org.tym.practice.ch1;
 
 /**
  * Created by tangym on 2016/11/2.
@@ -6,15 +6,8 @@ package org.tym.practice;
 public class Q_1_1_1 {
 
     public static void main(String[] args) {
-        // 1.1.1
         System.out.println((0 + 15) / 2);
         System.out.println(2.0e-6 * 10000000000.1);
         System.out.println(true && false || true && true);
-
-        //1.1.2
-        System.out.println((1 + 2.236) / 2);
-        System.out.println(1 + 2 + 3 + 4.0);
-        System.out.println(4.1 >= 4);
-        System.out.println(1 + 2 + "3");
     }
 }
